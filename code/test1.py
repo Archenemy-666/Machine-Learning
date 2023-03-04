eb4adfdebaf30fafb2a4e7d276f8d2b5a90f82e6
Archenemy_666 <csiddu98@gmail.com>
2023-03-04 15:32:43 -0600

    plot complete

--- code/test1.py
+++ code/test1.py
@@ -1,5 +1,28 @@
-from sklearn import datasets , tree, model_selection
+from sklearn import datasets
+from sklearn import tree
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from sklearn import metrics
+from sklearn import model_selection
+
+dia = datasets.fetch_openml(data_id = 45086)
+mytree = tree.DecisionTreeClassifier(criterion="entropy")
+
+mytree.fit(dia.data,dia.target)
+
+parameters = [{"min_samples_leaf":[1,4,6,12]}]
 dtc = tree.DecisionTreeClassifier()
-tuned_dtc = model_selection.GridSearchCV(dtc, parameters, scoring = "roc_auc", cv=5 )
-cv = model_selection.cross_validate(tuned_dtc, dia.data,dia.target, scoring = ["roc_auc", "accuracy"], cv =10)
-cv 
+tuned_dtc = model_selection.GridSearchCV(dtc, parameters, scoring="roc_auc", cv=10)
+cv = model_selection.cross_validate(tuned_dtc, dia.data, dia.target, scoring="roc_auc", cv=10,return_train_score=True)
+print(cv["test_score"].mean())
+
+tuned_dtc.fit(dia.data, dia.target)
+print(tuned_dtc.best_params_)
+
+dtc2 = tree.DecisionTreeClassifier()
+cv2 = model_selection.cross_validate(tuned_dtc,dia.data,dia.target,scoring=["accuracy","roc_auc"],cv=10)
+accuracy_mean = cv2["test_accuracy"].mean()
+test_roc_auc_mean = cv2["test_roc_auc"].mean()
+print(accuracy_mean,test_roc_auc_mean)
+print(cv2["test_accuracy"],cv2["test_accuracy"])
---
+++ course.md
@@ -0,0 +1,6 @@
+march 15 test 
+mock available
+portion until march 8th class 
+no sklearn or programming
+closed book 
+in-person
